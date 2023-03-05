c6b3e5cfc704b66d1d41c7815ae0c9cdda48e003
RobAnt13 <116080051+RobAnt13@users.noreply.github.com>
2023-03-05 17:03:04 +0100

    Add files via upload

---
+++ server/sampleData.js
@@ -0,0 +1,74 @@
+// Projects
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name: 'eCommerce Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        status: 'In Progress',
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name: 'Dating App',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        status: 'In Progress',  
+    },
+    {
+        id: '3',
+        clientId: '3',
+        name: 'SEO Project',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        status: 'In Progress',
+    },
+    {
+        id: '4',
+        clientId: '4',
+        name: 'Design Prototype',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        status: 'In Progress',
+    },
+    {
+        id: '5',
+        clientId: '5',
+        name: 'Auction Website',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        status: 'In Progress',
+    },
+];
+
+// Clients
+const clients = [
+    {
+        id: '1',
+        name: 'Tony Stark',
+        email: 'ironman@gmail.com',
+        phone: '343-567-433',
+    },
+    {
+        id: '2',
+        name: 'Natasha Romanova',
+        email: 'blackwidow@gmail.com',
+        phone: '343-567-334',
+    },
+    {
+        id: '3',
+        name: 'Thor Odinson',
+        email: 'thor@gmail.com',
+        phone: '343-567-122',
+    },
+    {
+        id: '4',
+        name: 'Steve Rogers',
+        email: 'captainamerica@gmail.com',
+        phone: '343-567-221',
+    },
+    {
+        id: '5',
+        name: 'Bruce Banner',
+        email: 'hulk@gmail.com',
+        phone: '343-567-111',
+    },
+];
+
+module.exports = { projects, clients };
